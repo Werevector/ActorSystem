7b2fe40127ab0e8f897db08876068fa6514d9641
Runar Tistel <runartistel@gmail.com>
2016-06-22 11:30:42 +0200

    initial commit of main

--- source/main.cpp
+++ source/main.cpp
@@ -4,6 +4,10 @@
 
 int main(int argc, char* argv[])
 {
+	int win_height = 1080 / 2;
+	int win_width = 1920 / 2;
+	bool quit = false;
+
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_version compiled;
 	SDL_version linked;
@@ -15,6 +19,34 @@ int main(int argc, char* argv[])
 	printf("But we are linking against SDL version %d.%d.%d.\n",
 		linked.major, linked.minor, linked.patch);
 
-	system("pause");
+	
+
+	SDL_Window* window_handle = SDL_CreateWindow("ActorSystem", 100, 100, win_width, win_height, SDL_WINDOW_SHOWN);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window_handle, -1, SDL_RENDERER_ACCELERATED);
+
+	SDL_Rect rect; rect.x = (win_width / 2) - 30; rect.y = (win_height / 2) - 30; rect.w = 30; rect.h = 30;
+
+	
+	
+	SDL_Event* event_handle = new SDL_Event();
+	while (!quit) {
+		while (SDL_PollEvent(event_handle) != NULL) {
+			if (event_handle->type == SDL_QUIT) {
+				quit = true;
+			}
+		}
+
+		SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+		SDL_RenderClear(renderer);
+
+		SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
+		SDL_RenderDrawRect(renderer, &rect);
+		SDL_RenderPresent(renderer);
+	}
+
+	std::cout << "Quitting program. \n";
+
+
+
 	return 0;
 }
